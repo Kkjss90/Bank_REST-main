bb5dbaec29ddf9733f0235c337a299296e20890a
Kkjss90 <122020808+Kkjss90@users.noreply.github.com>
2025-09-08 12:59:16 +0300

    docker and README_Bank_rest.md

---
+++ infrastructure/create_db.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS bank;
+create database bank;
--- src/main/java/com/example/bankcards/repository/TransactionRepository.java
+++ src/main/java/com/example/bankcards/repository/TransactionRepository.java
@@ -4,22 +4,22 @@ import com.example.bankcards.entity.Transaction;
 import com.example.bankcards.entity.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    @Query(value = "select * from transaction"
-            + "where ", nativeQuery = true)
-    List<Transaction> findByFromAccountUserId(Long userId);
-    @Query(value = "select * from transaction"
-            + "where ", nativeQuery = true)
-    List<Transaction> findByToAccountUserId(Long userId);
-    @Query(value = "select * from transaction"
-            + "where ", nativeQuery = true)
-    List<Transaction> findByFromAccountUserIdOrToAccountUserId(Long fromUserId, Long toUserId);
-    @Query(value = "select * from transaction"
-            + "where ", nativeQuery = true)
-    List<Transaction> findByStatus(TransactionStatus status);
+    @Query("SELECT t FROM Transaction t WHERE t.fromCard.user.id = :userId")
+    List<Transaction> findByFromAccountUserId(@Param("userId") Long userId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.toCard.user.id = :userId")
+    List<Transaction> findByToAccountUserId(@Param("userId") Long userId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.fromCard.user.id = :fromUserId OR t.toCard.user.id = :toUserId")
+    List<Transaction> findByFromAccountUserIdOrToAccountUserId(@Param("fromUserId") Long fromUserId, @Param("toUserId") Long toUserId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.status = :status")
+    List<Transaction> findByStatus(@Param("status") TransactionStatus status);
 }
